9a5620d919c802140116a0c99bdec12cec564983
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2024-03-21 21:20:12 -0600

    Add principal structure for navbar and footer from tailwind examples 💻

---
+++ varachess/app/[lng]/components/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function Page({
+    children, params:{lng},
+}: {children: ReactNode, params: {lng:string}}) {
+    return(
+        <>
+            <Navbar/>
+            <main>
+                {children}
+            </main>
+            <Footer/>
+        </>
+    )
+}
