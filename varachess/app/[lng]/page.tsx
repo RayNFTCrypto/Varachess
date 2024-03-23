9c8f48f0428a6d1a3ce2e69dd2c78d20d648415e
Raymon <88008457+RayNFTCrypto@users.noreply.github.com>
2024-03-23 13:01:55 -0600

    Add some seccions for principal page

--- varachess/app/[lng]/page.tsx
+++ varachess/app/[lng]/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import MainLayout from "./components/MainLayout";
+import { TrophyIcon } from "@heroicons/react/24/outline";
 
 interface PageProps {
   params: {
@@ -7,119 +8,162 @@ interface PageProps {
   };
 }
 
-export default function  Page({ params: { lng } }: PageProps) {
+export default function Page({ params: { lng } }: PageProps) {
   return (
     <MainLayout params={{
       lng: lng,
     }}>
+      {/* Hero section */}
+      <main className="flex min-h-screen flex flex-col md:flex-row justify-center items-center max-w-7xl mx-auto">
+        <div className="basis-1/2 px-5">
+          <h1 className="font-sans font-semibold text-white text-3xl md:text-6xl">The new era of chess built in Vara Network</h1>
+          <p className="mt-2 md:mt-5 font-sans font-light text-lg md:text-2xl">Dive into one of the most exciting games of all time.</p>
+          <div className="mt-5 grid grid-cols-1 md:grid-cols-2 gap-y-3 md:gap-y-0 md:gap-x-3 items-center justify-center w-full">
+              <a href="#" className="rounded-xl bg-cyan-700 py-3 text-center w-full">Play Now</a>
+              <a href="#" className="rounded-xl border-2 border-white py-3 text-center w-full">Learn more</a>
+          </div>
+        </div>
+        <div className="basis-1/2 px-5 my-5">
+          <Image className="object-cover" src={"/chess woman.png"} width={600} height={480} alt="Chess Woman"/>
+        </div>
+      </main>
+      {/* Info section */}
+      <main className="min-h-screen max-w-7xl mx-auto flex flex-col items-center justify-center">
+        <h2 className="font-sans font-semibold text-white text-2xl md:text-4xl text-center">
+          Explore VaraChess
+        </h2>
+        <div className = "mt-5 md:mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
+          <div className="border-2 border-white rounded-xl px-5 py-3">
+            <div className="flex flex-col items-center justify-center">
+            <TrophyIcon className="h-12"/>
+            <h4 className="font-sans font-semibold text-lg md:text-xl text-center">
+              Bet and win your Vara tokens
+            </h4>
+            <p className="font-light font-sans text-base md:text-lg text-center">
+              Play with your friends and inmerse yourself in the adventure of chess.
+            </p>
+            </div>
+          </div>
+          <div className="border-2 border-white rounded-xl px-5 py-3">
+            <div className="flex flex-col items-center justify-center">
+            <TrophyIcon className="h-12"/>
+            <h4 className="font-sans font-semibold text-lg md:text-xl text-center">
+              Bet and win your Vara tokens
+            </h4>
+            <p className="font-light font-sans text-base md:text-lg text-center">
+              Play with your friends and inmerse yourself in the adventure of chess.
+            </p>
+            </div>
+          </div>
+          <div className="border-2 border-white rounded-xl px-5 py-3">
+            <div className="flex flex-col items-center justify-center">
+            <TrophyIcon className="h-12"/>
+            <h4 className="font-sans font-semibold text-lg md:text-xl text-center">
+              Bet and win your Vara tokens
+            </h4>
+            <p className="font-light font-sans text-base md:text-lg text-center">
+              Play with your friends and inmerse yourself in the adventure of chess.
+            </p>
+            </div>
+          </div>
+          <div className="border-2 border-white rounded-xl px-5 py-3">
+            <div className="flex flex-col items-center justify-center">
+            <TrophyIcon className="h-12"/>
+            <h4 className="font-sans font-semibold text-lg md:text-xl text-center">
+              Bet and win your Vara tokens
+            </h4>
+            <p className="font-light font-sans text-base md:text-lg text-center">
+              Play with your friends and inmerse yourself in the adventure of chess.
+            </p>
+            </div>
+          </div>
+        </div>
+      </main>
+      <main className=""><div></div></main>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+            src="/varachess_logo.png"
+            alt="Varachess Logo Logo"
+            width={600}
+            height={10}
+            priority
+          />
+        </div>
+
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/varachess_logo.png"
-              alt="Varachess Logo"
-              className=""
-              width={100}
-              height={24}
-              priority
-            />
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Docs{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Find in-depth information about Next.js features and API.
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Learn{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Learn about Next.js in an interactive course with&nbsp;quizzes!
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Templates{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Explore starter templates for Next.js.
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Deploy{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+              Instantly deploy your Next.js site to a shareable URL with Vercel.
+            </p>
           </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/varachess_logo.png"
-          alt="Varachess Logo Logo"
-          width={600}
-          height={10}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
     </MainLayout>
-    
+
   );
 }
